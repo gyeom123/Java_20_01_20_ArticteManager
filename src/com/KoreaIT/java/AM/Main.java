2c0752ebeaa930f1391deae66451f7ebb6f0d7cc
gyeom123 <gyeomju6536@gmail.com>
2023-01-31 19:24:18 +0900

    게시물 삭제기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -95,6 +95,35 @@ public class Main {
 					System.out.printf("제목 : %s\n", foundArticle.title);
 					System.out.printf("내용 : %s\n", foundArticle.body);
 				}
+			} else if (cmd.startsWith("article delete ")) {
+				String[] cmdBits = cmd.split(" ");
+			
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+				int 게시물삭제 = 0;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						게시물삭제 = id;
+						foundArticle = article;
+						break;// 가장 가까운 반복문 종료
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				
+				// size() => 3 
+				// index : 0 1 2
+				// id    : 1 2 3
+				
+				articles.remove(게시물삭제);
+				System.out.printf("%d번 게시물이 삭제 되었습니다\n", 게시물삭제);
 
 			}
 
